404065881266c4050b042b1af4b2f0c735f2b619
Jesus Fabian <pondering.dev1@gmail.com>
2020-03-18 10:27:07 -0400

    connected custom cell to collection VC

---
+++ StashCoach_Challenge/StashCoach_Challenge/CustomCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCollectionViewCell.swift
+//  StashCoach_Challenge
+//
+//  Created by Jesus Fabian on 3/18/20.
+//  Copyright © 2020 Jesus Fabian. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
