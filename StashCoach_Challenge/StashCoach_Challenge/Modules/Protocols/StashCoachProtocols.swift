d699f32b7deb74548a6042188a9ccd762c44fc21
Jesus Fabian <pondering.dev1@gmail.com>
2020-03-18 17:25:22 -0400

    protocol file complete

---
+++ StashCoach_Challenge/StashCoach_Challenge/Modules/Protocols/StashCoachProtocols.swift
@@ -0,0 +1,35 @@
+//
+//  StashCoachProtocols.swift
+//  StashCoach_Challenge
+//
+//  Created by Jesus Fabian on 3/18/20.
+//  Copyright © 2020 Jesus Fabian. All rights reserved.
+//
+
+import UIKit
+
+
+
+protocol PresenterToViewProtocol: class {
+    func displayCollectionView()
+    func displayError()
+}
+
+protocol InteractorToPresenterProtocol : class{
+    func achievementsFetched()
+    func achievementsFetchFailed()
+}
+
+protocol PresenterToInteractorProtocol: class{
+    func readJSON()
+    var presenter: InteractorToPresenterProtocol?{get set}
+}
+protocol ViewToPresenterProtocol: class {
+    var view: PresenterToViewProtocol?{ get set}
+    var interactor: PresenterToInteractorProtocol?{get set}
+    var router: PresenterToRouterProtocol?{get set}
+    func updateView()
+}
+protocol PresenterToRouterProtocol: class {
+    func setUp() -> UIViewController
+}
