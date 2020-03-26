15d94578d66af7fbb423b1c17948188a0d72c64d
Jesus Fabian <pondering.dev1@gmail.com>
2020-03-26 15:48:24 -0400

    data will belong to presenter

--- StashCoach_Challenge/StashCoach_ChallengeTests/StashCoach_ChallengeTests.swift
+++ StashCoach_Challenge/StashCoach_ChallengeTests/StashCoach_ChallengeTests.swift
@@ -41,6 +41,8 @@ class StashCoach_ChallengeTests: XCTestCase {
     
     func testInteractorReadJSON(){
         
+        ///TODO. instead of checking view check the presenter data
+        
         // Interactor reads from invalid path
         presenter.interactor?.readJSON("invalid file path", "json")
         XCTAssertNil(presenter.view?.data)
@@ -58,12 +60,35 @@ class StashCoach_ChallengeTests: XCTestCase {
     func testAchievementsFetched(){
         presenter.achievementsFetched(nil)
          XCTAssertNil(presenter.view?.data)
+        
+        let model1 = AchievementModel(id: 10, level: "10", progress: 3, total: 100, bg_image_url: "someurl.jpg", accessible: true)
+        let model2 = AchievementModel(id: 33, level: "4", progress: 4, total: 4, bg_image_url: "another_url.jpg", accessible: false)
+        var testingList = [AchievementModel]()
+        testingList.append(model1)
+        let dummyOverview = AchievementsResponse.Overview(title: "Dummy overview")
+        let dummyresponse = AchievementsResponse(success: true, status: 200, overview: dummyOverview, achievements: testingList)
+        
+        presenter.achievementsFetched(dummyresponse)
+        
+//        var collectionVC = StashCoachCollectionViewController()
+//        presenter.view = collectionVC
+//        collectionVC.list = presenter.view?.data.
+//        XCTAssertEqual(presenter.view?., <#T##expression2: FloatingPoint##FloatingPoint#>, accuracy: <#T##FloatingPoint#>)
+        
 
     }
     
     
-
-
+    func testDisplayCollectionView() {
+        
+        //assert vc nil
+        //call displaycollectionview
+        
+        // test list is not nil
+        // test list however many elt's i specify
+    }
+    
+    
 //    func testPerformanceExample() {
 //        // This is an example of a performance test case.
 //        measure {
