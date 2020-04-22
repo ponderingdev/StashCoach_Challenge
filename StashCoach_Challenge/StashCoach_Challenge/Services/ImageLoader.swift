f7ef471005faf4d03282de011f9c7940f762f240
Jesus Fabian <pondering.dev1@gmail.com>
2020-04-22 10:27:14 -0400

    one completion block instead of 2

--- StashCoach_Challenge/StashCoach_Challenge/Services/ImageLoader.swift
+++ StashCoach_Challenge/StashCoach_Challenge/Services/ImageLoader.swift
@@ -16,6 +16,7 @@ class ImageLoader: NSObject {
     
     
     
+    //TODO: make one block, two is not neccary
                    
     func getImage(_ url:URL, success: @escaping(UIImage)->Void, failure: @escaping(UIImage)->Void){
         
@@ -39,6 +40,22 @@ class ImageLoader: NSObject {
         }
         dataTask?.resume()
         
+    }
+    
+    func loadImage(_ url:URL, completion: @escaping(UIImage)->Void){
+        
+        if let image = loadedImages?[url]{
+            completion(image)
+        }
+            
+        dataTask?.cancel()
+        dataTask = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+            guard let data = data, error == nil else {
+                completion(UIImage(named: "stash_challenge_sad_cat")!)
+                return
+            }
+            completion(UIImage(data: data)!)
+        })
     }
     
 
