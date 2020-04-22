3397e4d65e3946a32c14fe7ae4dd7e0bc135e87b
Jesus Fabian <pondering.dev1@gmail.com>
2020-04-21 21:39:04 -0400

    getImage will have a uiimage in its completion block

--- StashCoach_Challenge/StashCoach_Challenge/Services/ImageLoader.swift
+++ StashCoach_Challenge/StashCoach_Challenge/Services/ImageLoader.swift
@@ -13,5 +13,41 @@ class ImageLoader: NSObject {
     var dataTask: URLSessionTask?
 //    will create simple 'cache' to store url and image
     var loadedImages: [URL: UIImage]?
+    
+    
+                   
+                   
+    func getImage(_ url:URL, success: @escaping(UIImage)->Void){
+        
+//        let imgUrl = URL(string: url)!
+        
+        /// first look in the dictionary
+        
+        if let image = loadedImages?[url]{
+            success(image)
+        }
+        
+        dataTask?.cancel()
+        dataTask = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            
+//            success(data)
+        }
+        dataTask?.resume()
+        
+//        dataTask  = URLSession.shared.dataTask(with: imgUrl) { (data, response, error) in
+//                guard let data = data else { return }
+//            DispatchQueue.main.async {
+//                completion(data
+//                )
+//            }
+//        }
+//        task.resume()
+//
+    }
+    
 
 }
